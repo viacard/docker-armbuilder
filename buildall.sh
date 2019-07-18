9d3c316cfe0f2f32f4cb80e140e0afa712e346a7
SmallRoomLabs <mats.engstrom@gmail.com>
2019-07-18 17:10:48 +0200

    Handle build/push/delete. Don't use :latest

--- buildall.sh
+++ buildall.sh
@@ -3,40 +3,58 @@
 BASENAME=viacard/armbuilder
 VERSION=v1
 
-if [[ "$1" == "build" ]]; then
-  echo Untarring atmel.tar.bz2
-  tar -xf atmel.tar.bz2
-fi
-
 declare -a samtags=(
-    "SAM4E"     "SAMG"     "SAMDA1"   "SAM4S"    "SAMR30"    
-    "SAM4C"     "SAMR21"   "SAM3U"    "SAML21"   "SAMB11"  
-    "SAM3A"     "SAML22"   "SAM3S"    "SAMD11"   "SAMD10"  
-    "SAME70"    "SAMD09"   "SAMD21"   "SAMD20"   "SAME54"    
-    "SAME53"    "SAM3N"    "SAML11"   "SAMV71"   "SAMV70" 
-    "SAML10"    "SAME51"   "SAM3X"    "SAM4L"    "SAMS70"  
+    "SAM4E"     "SAMG"     "SAMDA1"   "SAM4S"    "SAMR30"
+    "SAM4C"     "SAMR21"   "SAM3U"    "SAML21"   "SAMB11"
+    "SAM3A"     "SAML22"   "SAM3S"    "SAMD11"   "SAMD10"
+    "SAME70"    "SAMD09"   "SAMD21"   "SAMD20"   "SAME54"
+    "SAME53"    "SAM3N"    "SAML11"   "SAMV71"   "SAMV70"
+    "SAML10"    "SAME51"   "SAM3X"    "SAM4L"    "SAMS70"
     "SAM4N"     "SAMD51"   "SAMHA1"   "SAMC21"   "SAMC20"
 )
 
-for TAG in ${samtags[@]}; do
-  TAGLC=$(echo $TAG | tr "A-Z" "a-z")
-  if [[ "$1" == "build" ]]; then
+if [[ "$1" == "delete" ]]; then
+  docker images --format '{{.Repository}}:{{.Tag}}' | grep '^viacard/armbuilder*' > /dev/null
+  if [[ "$?" == "0" ]]; then
+    docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' | grep '^viacard/armbuilder*')
+  fi
+  docker image prune -f
+  exit 0
+fi
+
+
+if [[ "$1" == "build" ]]; then
+  echo Untarring atmel.tar.bz2
+  tar -xf atmel.tar.bz2
+  for TAG in ${samtags[@]}; do
+    TAGLC=$(echo $TAG | tr "A-Z" "a-z")
     echo " "
     echo ----------------------------------------
     echo NOW BUILDING $BASENAME:$TAGLC-$VERSION
     echo ----------------------------------------
-    docker build -f Dockerfile.SAM --build-arg proctype=$TAG --build-arg proctypelc=$TAGLC --tag $BASENAME:$TAGLC-latest --tag $BASENAME:$TAGLC-$VERSION .
-  fi
-  if [[ "$1" == "build" ]]; then
+    INC2VER=$(ls -1 atmel/${TAG}_DFP)
+    docker build -f Dockerfile.SAM --build-arg proctype=$TAG --build-arg proctypelc=$TAGLC --build-arg inc2ver=$INC2VER --tag $BASENAME:$TAGLC-$VERSION .
+  done
+  rm -rf atmel
+  exit 0
+fi
+
+
+if [[ "$1" == "push" ]]; then
+  for TAG in ${samtags[@]}; do
+    TAGLC=$(echo $TAG | tr "A-Z" "a-z")
     echo " "
     echo ----------------------------------------
     echo NOW PUSHING $BASENAME:$TAGLC-$VERSION
     echo ----------------------------------------
-    docker push $BASENAME:$TAGLC-latest 
     docker push $BASENAME:$TAGLC-$VERSION
-  fi
-done
-
-if [[ "$1" == "build" ]]; then
-  rm -rf atmel
+  done
+  exit 0
 fi
+
+
+echo Usage\:
+echo $0 \<command\>
+echo "   build"
+echo "   push"
+echo "   delete"
